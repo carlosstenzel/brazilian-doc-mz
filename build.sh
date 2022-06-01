02c29c4f7a7dfea2bdae11f1ce9417111d612381
Tiago França <tiago@tiagofranca.com>
2022-06-01 16:00:55 -0300

    build scripts
    No ramo feat/v2-tf2
    Mudanças a serem submetidas:
    	new file:   .gitignore
    	new file:   build.sh
    	new file:   build/.gitignore

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ROOT_DIR="$(pwd -P)"
+BUILD_DIR="./build"
+ZIP_FILE="${BUILD_DIR}/build.zip"
+
+if [ ! -d "${BUILD_DIR}" ]; then
+    mkdir -p "${BUILD_DIR}"
+fi
+
+if [ -f "${ZIP_FILE}" ]; then
+    rm "${ZIP_FILE}"
+fi
+
+# echo -e "${ZIP_FILE}"; exit 0;
+
+# zip -r ./build/build.zip .
+
+zip -r -FS "${ZIP_FILE}" * \
+    --exclude '*.git*' \
+    --exclude '*build**'
